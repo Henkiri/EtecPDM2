bdec4f02a96350ab1b3005d6e9e30dfd212a4695
Henrique Bernardino <henriquebernardinosilva@gmail.com>
2024-10-19 08:05:15 -0300

    A15_Exercicios.kt

--- src/main/kotlin/A15_Exercicios.kt
+++ src/main/kotlin/A15_Exercicios.kt
@@ -1,76 +1,82 @@
-/*
-    Aula 15: Exercícios de reforço
-    Controle de Fluxo: for()
- */
+ // Aula 15: Controle de fluxo exercicios
 
-// CONTROLE DE FLUXO - for()
+//Controle de Fluxo - for()
 fun main() {
     // crescente()
     // decrescente()
     // excetoM5()
     // somaSequencia()
-    // escada(9)
-    escadaIvertida(7)
+    // escada(5)
+    // escadaInvertida(7)
 }
 
+//1. imprimir os números de 1 a 50 na mesma linha em ordem crescente.
+// A saída esperada deve ser semelhante a esta: 1 2 3 4 5 6 7 8 9 10 ....... 49 50.
+// Você usará o loop for() para esta tarefa.
 
-// 1. Imprima os números de 1 a 50 na mesma linha em ordem crescente. Saída esperada:
-//      1 2 3 4 5 6 7 8 9 10 ....... 49 50. Use for()
 fun crescente(){
     for(i in 1..50){
-        print("$i ")
+        print("$i")
+    }
+
+}
+
+//2.Imprimirá os números de 1 a 50 na mesma linha em ordem decrescente usando o loop for().
+// A saída esperada deve ser: 50 49 48 47 46 45 ......... 3 2 1.
+
+fun decrescente() {
+    for (i in 50 downTo 1){
+        print("$1")
     }
 }
 
-// 2. Imprima os números de 1 a 50 na mesma linha em ordem decrescente usando for(). Saída esperada:
-//      50 49 48 47 46 45 ......... 3 2 1
-fun decrescente(){
-    for(i in 50 downTo 1){
-        print("$i ")
-    }
-}
+//3.sua próxima tarefa é imprimir os números de 1 a 50 na mesma linha em ordem crescente, excluindo os múltiplos de 5.
+// Você conseguirá isso usando o loop for().
 
-// 3. Usando a resolução do exercício 1, não imprima os números múltiplos de 5. Use for()
 fun excetoM5(){
     for(i in 1..50){
-        if(i % 5 != 0){
-            print("$i ")
+        if(i % 5 !=0){
+             print("$i")
+            }
         }
     }
-}
 
-// 4. Faça a soma de todos os números no intervalo de 1 a 500. Ao final imprima o valor da soma.
+//4.Para a quarta tarefa, você somará todos os números no intervalo de 1 a 500 usando o loop for().
+// No final, você imprimirá o valor da soma.
 fun somaSequencia(){
     var soma = 0
     for (i in 1..500){
-        // soma = soma + i
+        //soma = soma + i
         soma += i
     }
     println(soma)
 }
 
-/* 5. Escreva um programa capaz de receber um número inteiro N e imprimir uma escada de tamanho N usando o caracter cerquilha (#). Use for()
-      Exemplo: se a entrada for 5, teremos:
+/*5. Escrever um programa capaz de pegar um inteiro n e imprimir uma escada de tamanho n usando o caractere de libra (#).
+ Você usará o loop for() para esta tarefa. Por exemplo, se a entrada for 5, a saída deverá ser:
             #
             ##
             ###
             ####
             #####
+
  */
 fun escada(n: Int){
     for (i in 1..n){
-        for (j in 1..i){
-            print("#.")
+        for (j in 1..i) {
+            print("#-")
         }
         println()
+        }
     }
-}
 
-fun escadaIvertida(n: Int){
-    for (i in n downTo 1){
-        for (j in i downTo 1){
-            print("#.")
+// Teste aleátório escada invertida
+fun escadaInvertida(){
+    for (i in n downTo 1) {
+        for (j in 1..i) {
+            print("#-")
         }
         println()
+        }
     }
-}
+
